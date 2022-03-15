12456e4f3e8214a53ad4e2841ade36d75d1fa066
Diego <diegoalfer23@gmail.com>
2022-03-15 19:44:04 +0100

    Login with POD

--- webapp/src/paginas/Login.tsx
+++ webapp/src/paginas/Login.tsx
@@ -1,203 +1,104 @@
-import React, { useState, useReducer, useEffect } from 'react';
+import React, { useState, useReducer, useEffect, SyntheticEvent } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
-import { CardHeader } from '@mui/material';
+import { Autocomplete, CardHeader } from '@mui/material';
 import NavBar from '../components/navegacion/NavBar';
-
-
+import { LoginButton, Text, useSession, CombinedDataProvider, LogoutButton } from "@inrupt/solid-ui-react";
 
 const useStyles = makeStyles({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      width: 400,
-      marginTop: '20px'
-      
-    },
-    loginBtn: {
-      marginTop: '20px',
-      flexGrow: 1,
-      color: 'white'
-    },
-    header: {
-      textAlign: 'center',
-      background:  'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      color: '#fff'
-    },
-    card: {
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      marginTop: '20px'
-    },
-    cardContent: {
-      background: 'white'
-    }
-  });
-
-
-type State = {
-  username: string
-  password:  string
-  isButtonDisabled: boolean
-  helperText: string
-  isError: boolean
-};
-
-const initialState:State = {
-  username: '',
-  password: '',
-  isButtonDisabled: true,
-  helperText: '',
-  isError: false
-};
-
-type Action = { type: 'setUsername', payload: string }
-  | { type: 'setPassword', payload: string }
-  | { type: 'setIsButtonDisabled', payload: boolean }
-  | { type: 'loginSuccess', payload: string }
-  | { type: 'loginFailed', payload: string }
-  | { type: 'setIsError', payload: boolean };
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case 'setUsername': 
-      return {
-        ...state,
-        username: action.payload
-      };
-    case 'setPassword': 
-      return {
-        ...state,
-        password: action.payload
-      };
-    case 'setIsButtonDisabled': 
-      return {
-        ...state,
-        isButtonDisabled: action.payload
-      };
-    case 'loginSuccess': 
-      return {
-        ...state,
-        helperText: action.payload,
-        isError: false
-      };
-    case 'loginFailed': 
-      return {
-        ...state,
-        helperText: action.payload,
-        isError: true
-      };
-    case 'setIsError': 
-      return {
-        ...state,
-        isError: action.payload
-      };
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: 400,
+    marginTop: '20px'
+    
+  },
+  loginBtn: {
+    marginTop: '20px',
+    flexGrow: 1,
+    color: 'white'
+  },
+  header: {
+    textAlign: 'center',
+    background:  'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    color: '#fff'
+  },
+  card: {
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    marginTop: '20px'
+  },
+  cardContent: {
+    background: 'white',
   }
-}
+});
 
-const Login = () => {
+const authOptions = {
+  clientName: "DeDe",
+};
+
+const providers = [
+  { name: "Inrupt", value: "https://broker.pod.inrupt.com" },
+  { name: "SolidCommunity", value: "https://solidcommunity.net" },
+];
+
+
+export default function Login() {
+  const { session } = useSession();
   const classes = useStyles();
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [idp, setIdp] =useState("https://broker.pod.inrupt.com");
+  
 
-  useEffect(() => {
-    if (state.username.trim() && state.password.trim()) {
-     dispatch({
-       type: 'setIsButtonDisabled',
-       payload: false
-     });
-    } else {
-      dispatch({
-        type: 'setIsButtonDisabled',
-        payload: true
-      });
-    }
-  }, [state.username, state.password]);
 
-  const handleLogin = () => {
-    if (state.username === 'abc' && state.password === '1') {
-      dispatch({
-        type: 'loginSuccess',
-        payload: 'Ha iniciado sesión'
-      });
-    } else {
-      dispatch({
-        type: 'loginFailed',
-        payload: 'Usuario o contraseña incorrectos'
-      });
-    }
-  };
+return (
+  
+  
+  <div className="app-container">
+   {session.info.isLoggedIn ? (
+              <LogoutButton>
+              <Button id="LogoutButton" className={classes.loginBtn} data-testid="button" variant="contained" fullWidth size="large" color="secondary" >Log Out</Button>
+              </LogoutButton>
+    ) : (
+  <form  noValidate autoComplete="off">
+  <Card className={classes.card} variant="outlined">
+    <CardHeader className={classes.header} title="Identifícate" />
+    <CardContent className={classes.cardContent} >
+         
+    <TextField
+      id="idp"
+      label="IDP"
+      placeholder="Identity Provider"
+      fullWidth
+      margin="normal"
+      defaultValue={idp}
+      onChange={(e) => setIdp(e.target.value)}
+    />
+    <Autocomplete
+         options={providers}
+         getOptionLabel={(option) => option.name}
+         fullWidth
+         data-testid="provider"
+         renderInput={(params) => <TextField {...params} label="Provider" variant="outlined" margin="normal" />}
+         onChange={(event, o) =>setIdp(o!.value)}
+                        />
+    <LoginButton
+      oidcIssuer={idp}
+      redirectUrl={window.location.href}
+      onError={console.error}
+      authOptions={authOptions}
+    >
+       <Button id="LogInButton" className={classes.loginBtn} data-testid="button" variant="contained" fullWidth size="large" color="secondary" >Log In</Button>
+    </LoginButton>
+    </CardContent>
+  </Card>
+</form>
+  )}
+</div>
 
-  const handleKeyPress = (event: React.KeyboardEvent) => {
-    if (event.keyCode === 13 || event.which === 13) {
-      state.isButtonDisabled || handleLogin();
-    }
-  };
 
-  const handleUsernameChange: React.ChangeEventHandler<HTMLInputElement> =
-    (event) => {
-      dispatch({
-        type: 'setUsername',
-        payload: event.target.value
-      });
-    };
-
-  const handlePasswordChange: React.ChangeEventHandler<HTMLInputElement> =
-    (event) => {
-      dispatch({
-        type: 'setPassword',
-        payload: event.target.value
-      });
-    }
-  return (
-    
-      <form  noValidate autoComplete="off">
-        <Card className={classes.card} variant="outlined">
-          <CardHeader className={classes.header} title="Identifícate" />
-          <CardContent className={classes.cardContent} >
-            <div>
-              <TextField
-                error={state.isError}
-                fullWidth
-                id="username"
-                type="email"
-                label="Usuario"
-                placeholder="Usuario"
-                margin="normal"
-                onChange={handleUsernameChange}
-                onKeyPress={handleKeyPress}
-              />
-              <TextField
-                error={state.isError}
-                fullWidth
-                id="password"
-                type="password"
-                label="Contraseña"
-                placeholder="Contraseña"
-                margin="normal"
-                helperText={state.helperText}
-                onChange={handlePasswordChange}
-                onKeyPress={handleKeyPress}
-              />
-            </div>
-          </CardContent>
-          <CardActions>
-            <Button
-              variant="contained"
-              size="large"
-              color="secondary"
-              className={classes.loginBtn}
-              onClick={handleLogin}
-              disabled={state.isButtonDisabled}>
-              Login
-            </Button>
-          </CardActions>
-        </Card>
-      </form>
-    
-  );
+  
+);
 }
-
-export default Login;
