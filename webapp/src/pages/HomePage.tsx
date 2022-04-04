b949629411600ff2a91eed2b9230d1d1a394b36f
uo270497 <uo270497@uniovi.es>
2022-04-04 21:21:42 +0200

    "Primera versión del Filtro de productos"

--- webapp/src/pages/HomePage.tsx
+++ webapp/src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
-import { Container } from "@mui/material";
-import Cart from "../components/cart/Cart";
-import Login from "../components/Login";
-import NavBar from "../components/navegacion/NavBar";
 import Products from "../components/Products";
 import { Product } from "../shared/shareddtypes";
+
+import { useFormik } from 'formik';
+import { useState } from 'react';
+// material
+import { Stack, Typography } from '@mui/material';
+// components
+import Filter from "../components/filter/Filter";
+import ProductSort from "../components/filter/ProductSort";
 
 export type ProductAdd = {
   products: Product[]
@@ -12,8 +16,61 @@ export type ProductAdd = {
 
 
 export const HomePage= (props:ProductAdd)=> {
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      gender: '',
+      category: '',
+      colors: '',
+      priceRange: '',
+      rating: ''
+    },
+    onSubmit: () => {
+      setOpenFilter(false);
+    }
+  });
+
+  const { resetForm, handleSubmit } = formik;
+
+  const handleOpenFilter = () => {
+    setOpenFilter(true);
+  };
+
+  const handleCloseFilter = () => {
+    setOpenFilter(false);
+  };
+
+  const handleResetFilter = () => {
+    handleSubmit();
+    resetForm();
+  };
+
     return (
       <div className="HomePage">
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Products
+        </Typography>
+
+        <Stack
+          direction="row"
+          flexWrap="wrap-reverse"
+          alignItems="center"
+          justifyContent="flex-end"
+          sx={{ mb: 5 }}
+        >
+          <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
+            <Filter
+              formik={formik}
+              isOpenFilter={openFilter}
+              onResetFilter={handleResetFilter}
+              onOpenFilter={handleOpenFilter}
+              onCloseFilter={handleCloseFilter}
+            />
+            <ProductSort />
+          </Stack>
+        </Stack>
+
         <Products products={props.products} addToCart= {props.addToCart}/>   
       </div>
       
