e6234643baac88cc1df80c81363abc35ffda3d52
celia <uo277578@uniovi.es>
2022-04-06 18:31:50 +0200

    Coger webid para guardar pedidos

--- restapi/controllers/OrderController.ts
+++ restapi/controllers/OrderController.ts
@@ -3,19 +3,23 @@ require("../utils/connectDB")
 import { Request, Response } from 'express';
 import Order from '../models/Order';
 import Product from '../models/Product';
-import { useSession } from "@inrupt/solid-ui-react";
+
+import { createBrotliDecompress } from 'zlib';
 
 
 var randomstring = require("randomstring");
+
 
 export const findAllOrders = async (req: Request, res: Response): Promise<Response> => {
     const products = await Order.find({});
     return res.json(products);
 };
 
+
 export const addOrder = async (req: Request, res: Response) => {
     let order = new Order();
     order.code_order = randomstring.generate();
+    order.user_id = req.body.user_id;
     //order.products = req.body.cartProducts;
     order.price = req.body.price;
     order.direccion = req.body.direccion;
@@ -25,6 +29,4 @@ export const addOrder = async (req: Request, res: Response) => {
     await order.save();
     //return res.sendStatus(200);
   }
-
-
 
