281684ca0d4f25ddce56b64d78bc2109f2795ab3
celia <uo277578@uniovi.es>
2022-04-27 18:38:41 +0200

    Ajustar márgenes grid

--- webapp/src/components/products/Products.tsx
+++ webapp/src/components/products/Products.tsx
@@ -19,7 +19,7 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function Products(product:ProductAdd) {
   
   return (
-    <Grid container rowSpacing={5} marginTop={2} marginLeft={3}>
+    <Grid container rowSpacing={4} marginTop={2} marginLeft={3} marginBottom={4}>
     {product.products.map((p, i) => (
           <Grid item xs={2} sm={4} md={3} key={i} >
               <CardProduct product={p} addToCart= {product.addToCart}></CardProduct>
