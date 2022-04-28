13c1291fa23397fd58cdafdcdc492aa2c14fb5f0
celia <uo277578@uniovi.es>
2022-04-28 20:32:46 +0200

    Controlador y routa para obtener pagos

--- restapi/routes/apiPays.ts
+++ restapi/routes/apiPays.ts
@@ -3,6 +3,6 @@ import { findPayments } from "../controllers/PaysController";
 
 const paysRouter = express.Router()
 
-paysRouter.get("/payments", findPayments);
+paysRouter.get("/", findPayments);
 
 export default paysRouter;
