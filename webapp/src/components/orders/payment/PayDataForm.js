831d5ea71d6f13356c163e62ad4c7566a3c51dae
celia <uo277578@uniovi.es>
2022-04-28 22:02:41 +0200

    Formulario tarjeta de crédito

---
+++ webapp/src/components/orders/payment/PayDataForm.js
@@ -0,0 +1,32 @@
+const { FormGroup } = require("@material-ui/core");
+
+class CreditCard extends React.Component{
+ render(){
+    return (
+        <Modal>
+           <ModalHeader>Datos de tu tarjeta</ModalHeader> 
+           <ModalBody>
+               <FormGroup>
+                   <label>Número de tarjeta</label>
+                   <input type="text" value="Ej:5540 2345 1234 4456"></input>
+               </FormGroup>
+               <FormGroup>
+                   <label>Código de seguridad</label>
+                   <input type="text" value="Ej:448"></input>
+               </FormGroup>
+               <FormGroup>
+                   <label>Nombre del titular</label>
+                   <input type="text" value="María Álvarez Pérez"></input>
+               </FormGroup>
+               <FormGroup>
+                   <label>Fecha de caducidad</label>
+                   <input type="text" value="05/2023"></input>
+               </FormGroup>
+           </ModalBody>
+        </Modal>
+    );
+ }
+    
+
+
+}
--- webapp/src/components/orders/payment/PaymentItem.tsx
+++ webapp/src/components/orders/payment/PaymentItem.tsx
@@ -1,5 +1,9 @@
-import { Card , CardMedia} from "@mui/material";
+import { Button, Card , CardActionArea, CardActions, CardMedia, Dialog, IconButton} from "@mui/material";
+import { render } from "react-dom";
+import { Modal } from "reactstrap";
 import { PaymentType } from "../../../shared/shareddtypes";
+import ButtonAppBar from "../../navegacion/NavBar";
+
 
 type Payment = {
     paymentType: PaymentType
@@ -10,17 +14,21 @@ const PaymentItem: React.FC<Payment> = ({paymentType}) => {
     return (
         
         <Card sx={{ maxWidth: 150, height:100}}>
-            <CardMedia
-                component="img"
-                height="100"
-                width="150"
-                image={paymentType.img}
-                alt={paymentType.name}
-            />
+            <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="100"
+                        width="150"
+                        image={paymentType.img}
+                        alt={paymentType.name}
+                    />
+            </CardActionArea>
         </Card>
 
         
     );
 }
+
+
 
 export default PaymentItem;
