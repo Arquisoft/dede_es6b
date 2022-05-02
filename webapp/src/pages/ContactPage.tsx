bb446849c45189ec49fcb13b40018a53db634c77
celia <uo277578@uniovi.es>
2022-05-02 21:00:55 +0200

    Página de contacto

--- webapp/src/pages/ContactPage.tsx
+++ webapp/src/pages/ContactPage.tsx
@@ -9,9 +9,13 @@ export const ContactPage = (props:Props)=> {
           <p>CONTACTA CON NOSOTROS</p>
           <Grid container direction="column">
             <h4>EMAIL</h4>
-            
+            <p>Podemos responder a tus preguntas a través de contactDede@gmail.com </p>
             <h4>TELÉFONO</h4>
+            <p>Horario: </p>
+            <p>800 000 333 </p>
             <h4>REDES SOCIALES</h4>
+            <p>Twitter: dedeASW6b </p>
+            <p>Instagram: dedeASW6b </p>
           </Grid>   
         </div>
         
