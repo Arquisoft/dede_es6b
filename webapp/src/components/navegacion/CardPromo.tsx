73cde795eab473df32db5ad2fa1534468d51a692
celia <uo277578@uniovi.es>
2022-05-02 23:04:33 +0200

    Componente para parte de promociones

---
+++ webapp/src/components/navegacion/CardPromo.tsx
@@ -0,0 +1,41 @@
+import { Button, Card , CardActionArea, CardActions, CardContent, CardMedia, IconButton, Typography} from "@mui/material";
+import { render } from "react-dom";
+import { Box, FormGroup, Grid } from "@mui/material";
+
+type PromoProps = {
+    img: string,
+    title: string,
+    description:string
+}
+
+
+const CardPromo: React.FC<PromoProps> = ({img,title,description}) => {
+    return (
+        <div>
+        <Card sx={{ maxWidth: 150, height:100}}>
+            <CardActionArea >
+                    <CardMedia
+                        component="img"
+                        height="80"
+                        width="150"
+                        image={img}
+                        alt={title}
+                    />
+                    <CardContent >
+                    <Typography variant="body1" color="text.primary">
+                     {title}
+                    </Typography>
+                    <Typography variant="body1" color="text.primary">
+                     {description}
+                    </Typography>
+                  </CardContent>
+ 
+            </CardActionArea>
+        </Card>
+        </div>
+    );
+}
+
+
+
+export default PaymentItem;
