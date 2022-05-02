93a44d8541d722eb6c4334bd0c65c92827c395b8
celia <uo277578@uniovi.es>
2022-05-02 22:28:56 +0200

    Imagen promoción principal con css

--- webapp/src/pages/PromotionsPage.tsx
+++ webapp/src/pages/PromotionsPage.tsx
@@ -3,6 +3,7 @@ import { ContactWrapper } from "../components/navegacion/ContactOption.styles";
 import NavFilter, { Props } from "../components/navegacion/NavFilter"
 import promotionSeason from "../utils/img/promocion-temporada.jpeg"
 import { makeStyles } from '@material-ui/core/styles';
+import "./ImgPromo.css"
 
 
 
@@ -10,8 +11,8 @@ export const PromotionsPage = (props:Props)=> {
     return (
     <div>
         <NavFilter function={props.function} categorys={props.categorys}></NavFilter>
-
-        <div><img src={promotionSeason} alt={"primavera-verano"}/></div>
+        <p>DeDe / Promociones</p>
+        <Box sx={{ display: 'flex' , justifyContent: 'center'}}><img src={promotionSeason} alt={"primavera-verano"}/></Box>
 
     </div>
         
