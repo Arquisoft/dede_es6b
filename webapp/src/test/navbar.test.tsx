dbb2bec22fe4ac39e8959193abb3a5082f234c2f
celia <uo277578@uniovi.es>
2022-05-03 19:09:53 +0200

    Alguno tests webapp

--- webapp/src/test/navbar.test.tsx
+++ webapp/src/test/navbar.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Cart from '../components/cart/Cart';
 import NavBar from '../components/navegacion/NavBar';
 import NavLoggedOut from '../components/navegacion/NavLoggedOut';
@@ -7,12 +8,49 @@ import Products from '../components/products/Products';
 import { HomePage } from '../pages/HomePage';
 import { Product } from '../shared/shareddtypes';
 
+
 test('filtermenu is rendered', () => {
-    const component = render(<HomePage products={[]} addToCart={function (clickedItem: Product): void {
-        throw new Error('Function not implemented.');
-    } } categorys={[]} function={function (s: string): void {
+    const products: Product[] = [
+        {
+            _id: "Prueba",
+        name: "nombre_Prueba1",
+        code: "Prueba",
+        size: "S",
+        stock: 12,
+        category: "Camisetas",
+        color: "Azul",
+        price: 22,
+        imagen: "",
+        quantity: 1
+        },
+        {
+            _id: "Prueba2",
+        name: "nombre_Prueba2",
+        code: "Prueba2",
+        size: "S",
+        stock: 12,
+        category: "Camisetas",
+        color: "Azul",
+        price: 20,
+        imagen: "",
+        quantity: 1
+        }
+    ]
+
+
+    const component = render(<HomePage products={products} addToCart={()=>{}} categorys={[]} function={function (s: string): void {
         throw new Error('Function not implemented.');
     } }/>)
+
+
+    expect(component.getByText('nombre_Prueba1'))
+    expect(component.getByText('22€'))
+
+    expect(component.getByText('nombre_Prueba2'))
+    expect(component.getByText('20€'))
+
+    
+
     expect(component.container).toHaveTextContent('Sudaderas')
     expect(component.container).toHaveTextContent('Pantalones')
     expect(component.container).toHaveTextContent('Camisetas')
@@ -20,12 +58,27 @@ test('filtermenu is rendered', () => {
     expect(component.container).toHaveTextContent('Accesorios')
   });
 
-  /*test('navbar is rendered', () => {
-    const component = render(<NavLoggedOut getItems={0} function={function (): void {
+  test('navbar logout is rendered', () => {
+    const component = render(<BrowserRouter><NavLoggedOut getItems={0} function={function (): void {
         throw new Error('Function not implemented.');
-    } }></NavLoggedOut>)
-    expect(component.container).toHaveTextContent('DEDE')
-    expect(component.container).toHaveTextContent('Iniciar sesión')
-  })*/
+    } }></NavLoggedOut></BrowserRouter>)
+    expect(component.getByText("Iniciar sesión"));
+    expect(component.getByText("DeDe"));
+    expect(component.getByText("Atención al cliente"));
+    expect(component.getByText("Contáctanos"));
+    expect(component.getByText("Promociones"));
+  });
+
+  test('click button Iniciar sesión', () => {
+    const component = render(<BrowserRouter><NavLoggedOut getItems={0} function={function (): void {
+        throw new Error('Function not implemented.');
+    } }></NavLoggedOut></BrowserRouter>)
+
+    const mockHandler = jest.fn()
+    
+    const button= component.getByText("Iniciar sesión");
+    fireEvent.click(button)
+  });
+
 
   
